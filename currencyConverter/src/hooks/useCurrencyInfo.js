be839f2059ac5d917a7fc775d18887f7fdfa1746
vatsal prajapati <prajapativatsal555@gmail.com>
2024-11-09 11:04:11 +0530

    currency converter

---
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res[currency], "res");
+        setData(res[currency]);
+      });
+  }, [currency]);
+
+  console.log(data);
+  return data;
+}
+
+export default useCurrencyInfo;
