ca52e77048e8fafef87f9999016b1c21c03938e4
vatsal prajapati <prajapativatsal555@gmail.com>
2024-11-09 11:12:38 +0530

    context api

---
+++ miniContext/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import UserContextProvider from "./Context/UserContextProvider";
+
+function App() {
+  return (
+    <UserContextProvider>
+      <h1>react login page with context api</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
+}
+
+export default App;
---
+++ miniContext/src/components/Login.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import UserContext from "../Context/UserContext";
+
+function Login() {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const { setUser } = React.useContext(UserContext);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser({ username, password });
+  };
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="username"
+      />{" "}
+      <input
+        type="text"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="password"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
+
+export default Login;
